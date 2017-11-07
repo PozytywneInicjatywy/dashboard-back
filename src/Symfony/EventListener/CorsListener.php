0444759bfead76825263bef875eccaf4bc505c18
Jakub Bizewski <jakubbizewski@gmail.com>
2017-11-07 23:45:55 +0100

    Added CorsListener

---
+++ src/Symfony/EventListener/CorsListener.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PozytywneInicjatywy\Dashboard\Symfony\EventListener;
+
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class CorsListener
+{
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $route = $event->getRequest()->getRequestUri();
+
+        if (preg_match("/^\/api/", $route)) {
+            $response = $event->getResponse();
+            $response->headers->set("Access-Control-Allow-Origin", "*");
+
+            $event->setResponse($response);
+        }
+    }
+}
